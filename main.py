60382ded749f69a499d7c4f080c31eabaab6bbd8
zachmixcsu <160365801+zachmixcsu@users.noreply.github.com>
2024-03-31 20:11:55 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+rooms = {
+    'CSC101': '3004',
+    'CSC102': '4501',
+    'CSC103': '6755',
+    'NET110': '1244',
+    'COM241': '1411'
+}
+
+instructors = {
+    'CSC101': 'Haynes',
+    'CSC102': 'Alvarado',
+    'CSC103': 'Rich',
+    'NET110': 'Burke',
+    'COM241': 'Lee'
+}
+
+times = {
+    'CSC101': '8:00 a.m.',
+    'CSC102': '9:00 a.m.',
+    'CSC103': '10:00 a.m.',
+    'NET110': '11:00 a.m.',
+    'COM241': '1:00 p.m.'
+}
+
+
+course_number = input("Enter course number:")
+
+print(f'Room: {rooms.get(course_number)}')
+print(f'Instructor: {instructors.get(course_number)}')
+print(f'Time: {times.get(course_number)}')
